1af48fd249936063128324b353c5f01728c18bbf
steffan boodhoo <boodhoo100@gmail.com>
2023-11-28 12:00:52 -0400

    test script

--- nasc_gpt/test.js
+++ nasc_gpt/test.js
@@ -1,3 +1,12 @@
-window.onload( ev =>{
-    console.log('test.js loaded')
-})
+console.log('SCRIPT INCLUDED');
+window.onload = ev => {
+    // console.log('test.js loaded')
+    testPrint();
+}
+
+const testPrint = () => {
+    console.log(' EXTERNAL SCRIPT LOADED AND OWRKING !!!');
+    setTimeout(() => {
+        testPrint();
+    }, 2000)
+}
